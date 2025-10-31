97d434f4f37a143b8bc5bce1f5792b6e297c1586
Farzam Nazari <Farzamnazari.mec@gmail.com>
2025-10-31 02:04:33 +0100

    update and added hyper parameters to utlis.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -30,9 +31,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         
         for i in range(len(list(models))):
             model = list(models.values())[i]
-                
-            # train the model
-            model.fit(X_train, y_train)
+            para = param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)              # train the model with hyper parameters
+            model.fit(X_train,y_train)
+            
             
             y_train_pred = model.predict(X_train)
             
